72e08d850f5a157225030f3da4ebca556a01f996
Marco Acierno <marcoaciernoemail@gmail.com>
2023-04-28 14:40:14 +0200

    tests

--- passwordtest/settings.py
+++ passwordtest/settings.py
@@ -85,21 +85,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -124,9 +109,18 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.ScryptPasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
     "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 1,
+        },
+    },
 ]
--- users/signals.py
+++ users/signals.py
@@ -5,16 +5,18 @@ from django.dispatch import receiver
 User = get_user_model()
 
 @receiver(models.signals.pre_save, sender=User)
-def pre_save(*args, **kwargs):
+def personal_pre_save(*args, **kwargs):
     user = kwargs['instance']
-    user.prev_password = user.password
+    user.prev_password = User.objects.get(id=user.id).password
     print('pre-save: raw pass', user._password, 'user.password', user.password)
 
 
 @receiver(models.signals.post_save, sender=User)
-def pre_save(*args, **kwargs):
+def personal_post_save(*args, **kwargs):
     user = kwargs['instance']
-    print('post-save:', user._password, 'are hashes different?', user.prev_password != user.password,
-          'user.prev_password', user.prev_password,
-          'user.password', user.password)
-
+    print('post-save:',
+          'raw password', user._password,
+          'are hashes equal?', user.prev_password == user.password,
+          'user.prev_password is: ', user.prev_password,
+          'user.password is:', user.password
+    )
